b694bf16f12e224718532450682f77a332d5a932
Isaias Nascimento <isaias@webeasy.com.br>
2020-05-17 17:36:15 -0300

    visão de provider manual

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:shop/providers/counter_provider.dart';
 import 'package:shop/utils/app_routes.dart';
 import 'package:shop/views/product_detail_screen.dart';
 import 'package:shop/views/products_overview_screen.dart';
@@ -12,17 +11,15 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return CounterProvider(
-      child: MaterialApp(
-          title: 'Minha loja',
-          theme: ThemeData(
-            primarySwatch: Colors.purple,
-            accentColor: Colors.deepOrange,
-            visualDensity: VisualDensity.adaptivePlatformDensity,
-            fontFamily: 'Lato',
-          ),
-          home: ProductOverviewScreen(),
-          routes: {AppRoutes.PRODUCT_DETAILS: (ctx) => ProductDetailScreen()}),
-    );
+    return MaterialApp(
+        title: 'Minha loja',
+        theme: ThemeData(
+          primarySwatch: Colors.purple,
+          accentColor: Colors.deepOrange,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+          fontFamily: 'Lato',
+        ),
+        home: ProductOverviewScreen(),
+        routes: {AppRoutes.PRODUCT_DETAILS: (ctx) => ProductDetailScreen()});
   }
 }
--- lib/views/product_detail_screen.dart
+++ lib/views/product_detail_screen.dart
@@ -1,13 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:shop/models/product.dart';
-import 'package:shop/providers/counter_provider.dart';
 
-class ProductDetailScreen extends StatefulWidget {
-  @override
-  _ProductDetailScreenState createState() => _ProductDetailScreenState();
-}
-
-class _ProductDetailScreenState extends State<ProductDetailScreen> {
+class ProductDetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final Product product =
@@ -15,20 +9,6 @@ class _ProductDetailScreenState extends State<ProductDetailScreen> {
     return Scaffold(
       appBar: AppBar(
         title: Text(product.title),
-      ),
-      body: Column(
-        children: <Widget>[
-          Text(CounterProvider.of(context)?.state?.value.toString()),
-          RaisedButton(
-            onPressed: () {
-              setState(() {
-                CounterProvider.of(context)?.state?.inc();
-              });
-              print(CounterProvider.of(context)?.state?.value);
-            },
-            child: Text('+'),
-          )
-        ],
       ),
     );
   }
