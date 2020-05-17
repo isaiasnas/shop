f3b5da4252b43bde721ddc0829ce205cf142c1a9
Isaias Nascimento <isaias@webeasy.com.br>
2020-05-17 16:50:23 -0300

    add image product

---
+++ lib/views/product_item.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:shop/models/product.dart';
+
+class ProductItem extends StatelessWidget {
+  final Product product;
+
+  const ProductItem(this.product);
+  @override
+  Widget build(BuildContext context) {
+    return GridTile(
+      child: Image.network(product.imageUrl),
+    );
+  }
+}
