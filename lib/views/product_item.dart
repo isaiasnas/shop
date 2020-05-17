4c73aca0f56cc01dbfe02b42476b49a33d43acb1
Isaias Nascimento <isaias@webeasy.com.br>
2020-05-17 16:54:16 -0300

    refatoração de grid produtos

--- lib/views/product_item.dart
+++ lib/views/product_item.dart
@@ -8,7 +8,25 @@ class ProductItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GridTile(
-      child: Image.network(product.imageUrl),
+      child: Image.network(
+        product.imageUrl,
+        fit: BoxFit.cover,
+      ),
+      footer: GridTileBar(
+        backgroundColor: Colors.black87,
+        leading: IconButton(
+          icon: Icon(Icons.favorite),
+          onPressed: () {},
+        ),
+        title: Text(
+          product.title,
+          textAlign: TextAlign.center,
+        ),
+        trailing: IconButton(
+          icon: Icon(Icons.shopping_cart),
+          onPressed: () {},
+        ),
+      ),
     );
   }
 }
