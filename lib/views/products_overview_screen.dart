7da15a4088edddf926fac9f43f47d58e73d78990
Isaias Nascimento <isaias@webeasy.com.br>
2020-05-17 22:27:20 -0300

    refatoração de products screen

--- lib/views/products_overview_screen.dart
+++ lib/views/products_overview_screen.dart
@@ -1,21 +1,22 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:shop/data/dummy_data.dart';
-import 'package:shop/models/product.dart';
+import 'package:provider/provider.dart';
+import 'package:shop/providers/products.dart';
 import 'package:shop/widgets/product_item.dart';
 
 class ProductOverviewScreen extends StatelessWidget {
-  final List<Product> loadedProducts = DUMMY_PRODUCTS;
   @override
   Widget build(BuildContext context) {
+    final productsProvider = Provider.of<Products>(context);
+    final products = productsProvider.items;
     return Scaffold(
       appBar: AppBar(
         title: Text('Minha Loja'),
       ),
       body: GridView.builder(
-        itemCount: loadedProducts.length,
+        itemCount: products.length,
         padding: const EdgeInsets.all(10),
-        itemBuilder: (ctx, index) => ProductItem(loadedProducts[index]),
+        itemBuilder: (ctx, index) => ProductItem(products[index]),
         gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
           crossAxisCount: 2,
           childAspectRatio: 3 / 2,
