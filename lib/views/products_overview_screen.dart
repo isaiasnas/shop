61003e5de652a02c6aafcd33cf3da3cfe83e464e
Isaias Nascimento <isaias@webeasy.com.br>
2020-05-17 23:05:50 -0300

    inclusão de consumer, otimizada

--- lib/views/products_overview_screen.dart
+++ lib/views/products_overview_screen.dart
@@ -2,12 +2,34 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:shop/views/product_grid.dart';
 
+enum FilterOptions {
+  Favorite,
+  All,
+}
+
 class ProductOverviewScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Minha Loja'),
+        actions: <Widget>[
+          PopupMenuButton(
+              onSelected: (FilterOptions selectedValue) {
+                print(selectedValue);
+              },
+              icon: Icon(Icons.more_vert),
+              itemBuilder: (_) => [
+                    PopupMenuItem(
+                      child: Text('Somente Favoritos'),
+                      value: FilterOptions.Favorite,
+                    ),
+                    PopupMenuItem(
+                      child: Text('Todos Favoritos'),
+                      value: FilterOptions.All,
+                    ),
+                  ])
+        ],
       ),
       body: ProductGrid(),
     );
