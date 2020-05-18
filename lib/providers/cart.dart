cfd7084c2e0f9397ffc3307f3f07c60b44e9bb9d
Isaias Nascimento <isaias@webeasy.com.br>
2020-05-18 13:58:06 -0300

    add class cartItem

---
+++ lib/providers/cart.dart
@@ -0,0 +1,13 @@
+class CartItem {
+  final String id;
+  final String title;
+  final int quantity;
+  final double price;
+
+  CartItem({
+    this.id,
+    this.title,
+    this.quantity,
+    this.price,
+  });
+}
