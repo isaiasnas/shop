2a614df354ea124c455500b4945b808bc3d4034c
Isaias Nascimento <isaias@webeasy.com.br>
2020-05-18 13:51:49 -0300

    refatoração de providers

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:shop/data/dummy_data.dart';
-import 'package:shop/models/product.dart';
+import 'package:shop/providers/product.dart';
 
 class Products with ChangeNotifier {
   List<Product> _items = DUMMY_PRODUCTS;
