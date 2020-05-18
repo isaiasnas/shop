498534f091c963b5c910ae4f60416e662b9dc1cc
Isaias Nascimento <isaias@webeasy.com.br>
2020-05-18 18:05:38 -0300

    refatoração crud produto

--- lib/views/products_screen.dart
+++ lib/views/products_screen.dart
@@ -2,11 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shop/providers/products.dart';
 import 'package:shop/widgets/app_drawer.dart';
+import 'package:shop/widgets/product_item.dart';
 
 class ProductsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final products = Provider.of<Products>(context);
+    final productItems = products.items;
     return Scaffold(
       appBar: AppBar(
         title: Text('Gerenciar Produtos'),
@@ -17,7 +19,9 @@ class ProductsScreen extends StatelessWidget {
         padding: EdgeInsets.all(8),
         child: ListView.builder(
           itemCount: products.itemsCount,
-          itemBuilder: (ctx, index) => Text('teste'),
+          itemBuilder: (ctx, index) => Column(
+            children: <Widget>[ProductItem(productItems[index]), Divider()],
+          ),
         ),
       ),
     );
---
+++ lib/widgets/product_item.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:shop/providers/product.dart';
+
+class ProductItem extends StatelessWidget {
+  final Product product;
+
+  const ProductItem(this.product);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text(product.title),
+      leading: CircleAvatar(
+        backgroundImage: NetworkImage(
+          product.imageUrl,
+        ),
+      ),
+      trailing: Container(
+        width: 100,
+        child: Row(
+          children: <Widget>[
+            IconButton(
+              icon: Icon(Icons.edit),
+              color: Theme.of(context).primaryColor,
+              onPressed: () {},
+            ),
+            IconButton(
+              icon: Icon(Icons.delete),
+              color: Theme.of(context).errorColor,
+              onPressed: () {},
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
