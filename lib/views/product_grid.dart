70ce2b7d3dcf43b0762e196142e9396c919638bc
Isaias Nascimento <isaias@webeasy.com.br>
2020-05-17 22:30:26 -0300

    add product_grid

---
+++ lib/views/product_grid.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop/providers/products.dart';
+import 'package:shop/widgets/product_item.dart';
+
+class ProductGrid extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final productsProvider = Provider.of<Products>(context);
+    final products = productsProvider.items;
+    return GridView.builder(
+      itemCount: products.length,
+      padding: const EdgeInsets.all(10),
+      itemBuilder: (ctx, index) => ProductItem(products[index]),
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        childAspectRatio: 3 / 2,
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10,
+      ),
+    );
+  }
+}
--- lib/views/products_overview_screen.dart
+++ lib/views/products_overview_screen.dart
@@ -1,29 +1,15 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:shop/providers/products.dart';
-import 'package:shop/widgets/product_item.dart';
+import 'package:shop/views/product_grid.dart';
 
 class ProductOverviewScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final productsProvider = Provider.of<Products>(context);
-    final products = productsProvider.items;
     return Scaffold(
       appBar: AppBar(
         title: Text('Minha Loja'),
       ),
-      body: GridView.builder(
-        itemCount: products.length,
-        padding: const EdgeInsets.all(10),
-        itemBuilder: (ctx, index) => ProductItem(products[index]),
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          childAspectRatio: 3 / 2,
-          crossAxisSpacing: 10,
-          mainAxisSpacing: 10,
-        ),
-      ),
+      body: ProductGrid(),
     );
   }
 }
